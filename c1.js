592eeed3701951a9ea9c3487f2ed8073df26481f
Fatimah Emad El-Den <63984422+xxfatj@users.noreply.github.com>
2021-12-23 23:32:51 +0300

    Add files via upload

---
+++ c1.js
@@ -0,0 +1,63 @@
+
+
+
+
+let heightt1 = 78;
+let masse1 = 1.69;
+
+let intBMIMark = masse1 / (heightt1* heightt1);
+
+
+let height0 = 92;
+let mass0 = 1.95;
+
+let intBMIJohn = mass0 / (height0 * height0 );
+
+//2nd test 
+
+let height1 = 95;
+let mass1 = 1.88;
+
+let secBMImark = mass1 / (height1 * height1);
+
+
+let height2 = 85;
+let mass2 = 1.55;
+
+
+
+let secBMIjhon = mass2 / (height2 * height2);
+
+let markHigherBMI = true ;
+
+
+//if mark's bmi is greater than jhon, print false
+
+        if ( intBMIMark  && secBMImark  > intBMIJohn  && secBMIjhon ) 
+
+            console.log(markHigherBMI );
+
+        else 
+
+            markHigherBMI = false;
+
+console.log(markHigherBMI);
+
+
+//console.log("hello");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
